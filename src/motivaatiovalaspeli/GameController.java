3776cdf9c42b59ce5a528925c8f09868be1dc3ca
Mikkomario <mikko_hilpinen@hotmail.com>
2012-12-14 16:31:25 +0200

    Continued to work with the gamecontroller

--- src/motivaatiovalaspeli/GameController.java
+++ src/motivaatiovalaspeli/GameController.java
@@ -3,6 +3,8 @@ package motivaatiovalaspeli;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import model.PhaseScreen;
+
 import handlers.DrawableHandler;
 
 /**
@@ -18,7 +20,18 @@ public class GameController implements KeyListener
 	private MotivaatiovalasPeli mvp;
 	private GamePhase phase;
 	private DrawableHandler drawer;
-	// TODO: Add phaseobjects
+	private PhaseScreen beginscreen;
+	private PhaseScreen pausescreen;
+	private PhaseScreen victoryscreen;
+	private PhaseScreen overscreen;
+	private PhaseScreen kssscreen;
+	
+	
+	// CONSTRUCTOR	------------------------------------------------------
+	
+	public GameController(MotivaatiovalasPeli peli, DrawableHandler phasedrawer)
+	{}
+	
 	
 	@Override
 	public void keyPressed(KeyEvent e)
